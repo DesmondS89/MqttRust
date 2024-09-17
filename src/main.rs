4f389c5c682f2a32a4f5f593593cbd401d691625
DeSmond-89 <schdra89@gmail.com>
2024-09-18 00:49:43 +0200

    Try to build project

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use anyhow;
+use embedded_svc::mqtt::client::Connection;
 use embedded_svc::mqtt::client::Event;
 use embedded_svc::mqtt::client::QoS;
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
-use embedded_svc::mqtt::client::Connection;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::mqtt::client::{EspMqttClient, MqttClientConfiguration};
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::web::{ApiEndpoint, EspWebServer, HttpMethod};
 use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
+use serde::{Deserialize, Serialize};
 use std::{thread::sleep, time::Duration};
 
-// Define Wifi SSID and Password
+// region variables
 const WIFI_SSID: &str = "";
 const WIFI_PASSWORD: &str = "";
 const MQTT_BROKER_URL: &str = "mqtt://6804c12a8e254e5c9f0d45b0ea9c0b2a.s1.eu.hivemq.cloud";
@@ -20,21 +22,25 @@ const MQTT_CLIENT_ID: &str = "esp32";
 const MQTT_USERNAME: &str = "hivemq.webclient.1726606709307";
 const MQTT_PASSWORD: &str = "9wD6c7YmrP<tIi!5VL#>";
 
-fn main() -> anyhow::Result<()> {
-    // It is necessary to call this function once. 
+// endregion
+
+fn main() -> Result<anyhow::Result<()>, _> {
+    // It is necessary to call this function once.
     // Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. 
+    // implemented by esp-idf-sys might not link properly.
     // See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_sys::link_patches();
+    // for more information.
+    // Try to uncomment the line below and see if the build fails.
+    // esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
     let sysloop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
     /// Initializes a mutable `wifi` instance by wrapping an `EspWifi` object with `BlockingWifi`.
-    /// 
-    /// The `EspWifi` object is created using the provided modem peripheral, system event loop, 
-    /// and an optional non-volatile storage (NVS) reference. The `BlockingWifi::wrap` function 
+    ///
+    /// The `EspWifi` object is created using the provided modem peripheral, system event loop,
+    /// and an optional non-volatile storage (NVS) reference. The `BlockingWifi::wrap` function
     /// is then used to create a blocking Wi-Fi instance, which is assigned to `wifi`.
     ///
     /// # Parameters
@@ -75,6 +81,74 @@ fn main() -> anyhow::Result<()> {
 
     println!("Wifi Connected");
 
+    // Wait for IP Address
+    while wifi.get_ip().unwrap().is_none() {
+        println!("Waiting for IP Address");
+        sleep(Duration::from_secs(1));
+    }
+
+    println!("IP Address: {:?}", wifi.get_ip().unwrap().unwrap());
+
+    // Create a Web Server
+    let mut server = EspWebServer::new(sysloop.clone())?;
+
+    // Start Web Server
+    server.start()?;
+    println!("Web Server Started");
+
+    // Create a Web Client
+    let mut client = EspWebClient::new(sysloop.clone())?;
+
+    // Create API Response
+    let response = ApiResponse {
+        status: 200,
+        message: "Hello World".into(),
+    };
+
+    // Create API Endpoint
+    let endpoint = ApiEndpoint {
+        method: HttpMethod::Get,
+    };
+
+    // Create an HTML Response with two buttons and send different messages to MQTT Broker
+    let html = r#"
+        <html>
+            <body>
+                <h1>ESP32 Web Server</h1>
+                <button onclick="send('Hello')">Hello</button>
+                <button onclick="send('World')">World</button>
+                <script>
+                    function send(message) {
+                        fetch('/api/hello', {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ message: message })
+                        });
+                    }
+                </script>
+            </body>
+        </html>
+    "#;
+
+    // Register HTML Endpoint
+    server.register("/", ApiEndpoint::new(HttpMethod::Get), move |req| {
+        req.send_html(html)
+    })?;
+
+    // Register API Endpoint
+    server.register("/api/hello", endpoint, move |req| {
+        let message = req.get_body::<ApiRequest>()?.message;
+        client.publish(
+            MQTT_TOPIC.into(),
+            QoS::AtLeastOnce,
+            false,
+            message.as_bytes(),
+        )?;
+        req.send_json(&response)
+    })?;
+
     // Create MQTT Client Configuration
     let mqtt_config = MqttClientConfiguration {
         broker_url: MQTT_BROKER_URL.into(),
@@ -97,27 +171,31 @@ fn main() -> anyhow::Result<()> {
     client.subscribe(MQTT_TOPIC.into(), QoS::AtLeastOnce)?;
 
     // Publish to MQTT Topic
-    client.publish(MQTT_TOPIC.into(), QoS::AtLeastOnce, false, "Hello World".as_bytes())?;
+    client.publish(
+        MQTT_TOPIC.into(),
+        QoS::AtLeastOnce,
+        false,
+        "Hello World".as_bytes(),
+    )?;
 
     // Event Loop
-    sysloop.run(|event| {
-        match event {
-            Event::Mqtt(client_event) => {
-                match client_event {
-                    Connection::Connected => {
-                        println!("Connected to MQTT Broker");
-                    }
-                    Connection::Disconnected => {
-                        println!("Disconnected from MQTT Broker");
-                    }
-                    Connection::MessageReceived(topic, message) => {
-                        println!("Received message on topic: {:?} with message: {:?}", topic, message);
-                    }
-                    _ => {}
-                }
+    sysloop.run(|event| match event {
+        Event::Mqtt(client_event) => match client_event {
+            Connection::Connected => {
+                println!("Connected to MQTT Broker");
+            }
+            Connection::Disconnected => {
+                println!("Disconnected from MQTT Broker");
+            }
+            Connection::MessageReceived(topic, message) => {
+                println!(
+                    "Received message on topic: {:?} with message: {:?}",
+                    topic, message
+                );
             }
             _ => {}
-        }
+        },
+        _ => {}
     })?;
 
     loop {
